504e2c96c89d5250798240174087d4dcacf54dbd
Nitisit Sooksang <nitisit.sooksang@sscinc.com>
2026-04-21 13:57:59 +0700

    update vite.config.js to use port 3000

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,13 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  preview: {
+    host: '0.0.0.0',
+    port: 3000,
+    allowedHosts: 'all',
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 3000,
+  },
 })
