1fd14f508aa089ad3deeb2ed6e984e656cbdd4a1
Nitisit Sooksang <nitisit.sooksang@sscinc.com>
2026-04-21 14:28:14 +0700

    reset config files.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,13 +4,4 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  preview: {
-    host: '0.0.0.0',
-    port: parseInt(process.env.PORT) || 3000,
-    allowedHosts: 'all',
-  },
-  server: {
-    host: '0.0.0.0',
-    port: parseInt(process.env.PORT) || 3000,
-  },
 })
