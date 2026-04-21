84774afbe031c17b17372f83e08e0fce78cec68c
Nitisit Sooksang <nitisit.sooksang@sscinc.com>
2026-04-21 14:06:07 +0700

    update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,11 +6,11 @@ export default defineConfig({
   plugins: [svelte()],
   preview: {
     host: '0.0.0.0',
-    port: 3000,
+    port: parseInt(process.env.PORT) || 3000,
     allowedHosts: 'all',
   },
   server: {
     host: '0.0.0.0',
-    port: 3000,
+    port: parseInt(process.env.PORT) || 3000,
   },
 })
